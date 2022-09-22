ff5393171ef26033490c0cb2d9946ac9cf5e1505
FeiyangZhangCode <eeefy@163.com>
2022-09-22 18:59:12 +0800

    20220922-M

--- AC-ActionTest.py
+++ AC-ActionTest.py
@@ -9,8 +9,6 @@ import datetime
 se = serial.Serial('/dev/ttyTHS1', 115200, timeout=0.09)
 # se = serial.Serial('COM6', 115200, timeout=0.09)
 
-str_fileAddress = './TestData/' + datetime.datetime.now().strftime('%H%M%S') + '-Record.txt'
-
 ultra_TH_F = 200  # 前向超声波阈值
 ultra_TH_B = 40  # 后向超声波阈值
 
@@ -77,101 +75,68 @@ def read_sensors(rec_mess):
     return fall_FL, fall_FR, fall_BL, fall_BR
 
 
-def single_action(hex_action):
-    sensor_state = 0  # 传感器状态，99是传感器异常，98是无反馈，97是运行报错
-    loop_nofeedback = 0  # 累加无反馈次数
-    threshold_nofeedback = 10  # 累计无反馈上限值
-    no_feedback = True
-    while no_feedback:
-        try:
-            se.write(hex_action)
-            str_send = binascii.b2a_hex(hex_action).decode('utf-8')
-            str_Time = datetime.datetime.now().strftime('%H:%M:%S.%f')
-            file_rec = open(str_fileAddress, 'a')
-            file_rec.write(str_Time + ',single,s,' + str_send + '\r\n')
-            file_rec.close()
-            print(str_Time, 's', str_send)
+def single_action(hex_action, single_time):
+    sensor_state = 0  # 传感器状态，99是传感器异常，98是无反馈
+    for single_num in range(0, single_time, 1):
+        loop_nofeedback = 0  # 累加无反馈次数
+        threshold_nofeedback = 10  # 累计无反馈上限值
+        se.write(hex_action)
+        data_1 = binascii.b2a_hex(hex_action)
+        print(data_1)
+        no_feedback = True
+        while no_feedback:
             cv2.waitKey(40)
             hex_rec = se.readline()
             if hex_rec:
                 # 收到反馈，跳出反馈循环
                 no_feedback = False
-                str_rec = binascii.b2a_hex(hex_rec).decode('utf-8')
-                if len(str_rec) >= 12:
-                    if str_rec[0:4] == 'aa02':
-                        sig_fall_FL, sig_fall_FR, sig_fall_BL, sig_fall_BR = read_sensors(str_rec)
-                        print('防跌落', sig_fall_FL, sig_fall_FR, sig_fall_BL, sig_fall_BR)
-                    else:
-                        print('头部异常', str_rec)
-                else:
-                    print('长度异常', str_rec)
-                file_rec = open(str_fileAddress, 'a')
-                file_rec.write(str_Time + ',single,r,' + str_rec + '\r\n')
-                file_rec.close()
+                str_rec = binascii.b2a_hex(hex_rec)
+                sig_fall_FL, sig_fall_FR, sig_fall_BL, sig_fall_BR = read_sensors(str_rec)
+                print('防跌落', sig_fall_FL, sig_fall_FR, sig_fall_BL, sig_fall_BR)
             else:
                 # 重新发送命令，并累计无反馈次数
+                se.write(hex_action)
                 loop_nofeedback += 1
-                print('无反馈', str(loop_nofeedback))
+                print('无反馈', loop_nofeedback)
                 if loop_nofeedback >= threshold_nofeedback:
                     sensor_state = 98
-                    no_feedback = False
-        except Exception as e_sa:
-            print(e_sa)
-            sensor_state = 97
-            no_feedback = False
+                    return sensor_state
     return sensor_state
 
 
 # 根据动作组进行执行，list的0是动作的16进制命令，1是执行多少次
 def func_action(list_action):
-    sensor_state = 0  # 传感器状态，99是传感器异常，98是无反馈，97是运行错误
+    sensor_state = 0  # 传感器状态，99是传感器异常，98是无反馈
     for id_action in range(0, len(list_action), 1):
         loop_nofeedback = 0  # 累加无反馈次数
         threshold_nofeedback = 10  # 累计无反馈上限值
         hex_action = list_action[id_action][0]
         time_action = list_action[id_action][1]
         for num_action in range(0, time_action, 1):
+            se.write(hex_action)
+            data_1 = binascii.b2a_hex(hex_action)
+            str_Time = datetime.datetime.now().strftime('%H:%M:%S.%f')
+            # print(str_Time)
+            # print(data_1)
             no_feedback = True
             while no_feedback:
-                try:
+                cv2.waitKey(60)
+                hex_rec = se.readline()
+                if hex_rec:
+                    # 收到反馈，跳出反馈循环
+                    no_feedback = False
+                    str_rec = binascii.b2a_hex(hex_rec)
+                    sensor_state = read_feedback(str_rec)
+                    print(str_rec)
+                    # if sensor_state > 0:
+                    #     return sensor_state, id_action
+                else:
+                    # 重新发送命令，并累计无反馈次数
                     se.write(hex_action)
-                    str_send = binascii.b2a_hex(hex_action).decode('utf-8')
-                    str_Time = datetime.datetime.now().strftime('%H:%M:%S.%f')
-                    file_rec = open(str_fileAddress, 'a')
-                    file_rec.write(str_Time + ',auto,s,' + str_send + '\r\n')
-                    file_rec.close()
-                    print(str_Time, 's', str_send)
-                    cv2.waitKey(60)
-                    hex_rec = se.readline()
-                    if hex_rec:
-                        # 收到反馈，跳出反馈循环
-                        no_feedback = False
-                        str_rec = binascii.b2a_hex(hex_rec).decode('utf-8')
-                        if len(str_rec) >= 12:
-                            if str_rec[0:4] == 'aa02':
-                                sig_fall_FL, sig_fall_FR, sig_fall_BL, sig_fall_BR = read_sensors(str_rec)
-                                print('防跌落', sig_fall_FL, sig_fall_FR, sig_fall_BL, sig_fall_BR)
-                            else:
-                                print('头部异常', str_rec)
-                        else:
-                            print('长度异常', str_rec)
-                        file_rec = open(str_fileAddress, 'a')
-                        file_rec.write(str_Time + ',auto,r,' + str_rec + '\r\n')
-                        file_rec.close()
-                        # if sensor_state > 0:
-                        #     return sensor_state, id_action
-                    else:
-                        # 重新发送命令，并累计无反馈次数
-                        loop_nofeedback += 1
-                        print('无反馈', str(loop_nofeedback))
-                        if loop_nofeedback >= threshold_nofeedback:
-                            sensor_state = 98
-                            return sensor_state, id_action
-                except Exception as e_fa:
-                    print(e_fa)
-                    sensor_state = 97
-                    return sensor_state, id_action
-
+                    loop_nofeedback += 1
+                    if loop_nofeedback >= threshold_nofeedback:
+                        sensor_state = 98
+                        return sensor_state, id_action
     return sensor_state, 0
 
 
@@ -296,55 +261,55 @@ if __name__ == '__main__':
         while in_init:
             str_init = 'aa 01 00 00 00 00 a8'
             hex_init_send = bytes.fromhex(str_init)
+            se.write(hex_init_send)
             str_Time = datetime.datetime.now().strftime('%H:%M:%S.%f')
-            try:
-                se.write(hex_init_send)
-                print(str_Time, 'i', str_init)
-                cv2.waitKey(40)
-                hex_init_rec = se.readline()
-                if hex_init_rec:
-                    str_init_rec = binascii.b2a_hex(hex_init_rec)
-                    if len(str_init_rec) >= 12:
-                        if str_init_rec[0:4].decode('utf-8') == 'aa02':
-                            str_fall_FL, str_fall_FR, str_fall_BL, str_fall_BR = read_sensors(str_init_rec)
-                            print('防跌落', str_fall_FL, str_fall_FR, str_fall_BL, str_fall_BR)
-                            in_init = False
-                            no_feedBack = False
-                            print('通信正常')
-                        else:
-                            print('数据包头部异常')
+            print(str_Time)
+            print(str_init)
+            cv2.waitKey(40)
+            hex_init_rec = se.readline()
+            if hex_init_rec:
+                str_init_rec = binascii.b2a_hex(hex_init_rec)
+                if len(str_init_rec) >= 12:
+                    if str_init_rec[0:4].decode('utf-8') == 'aa02':
+                        str_fall_FL, str_fall_FR, str_fall_BL, str_fall_BR = read_sensors(str_init_rec)
+                        print('防跌落', str_fall_FL, str_fall_FR, str_fall_BL, str_fall_BR)
+                        in_init = False
+                        no_feedBack = False
+                        print('通信正常')
                     else:
-                        print('数据包长度异常')
-            except Exception as e:
-                print(e)
+                        print('数据包头部异常')
+                else:
+                    print('数据包长度异常')
         print('进入动作测试')
 
         # 单个动作测试
         is_oneAction = True
         while is_oneAction:
+            str_Time = datetime.datetime.now().strftime('%H:%M:%S.%f')
+            print(str_Time)
             get_one_err = 0
             get_stop_err = 0
             # 输入单步命令
             if keyboard.is_pressed('w') or keyboard.is_pressed('8'):  # 向前移动，刮板和水系统停止
-                get_one_err = single_action(hex_moveFront)
+                get_one_err = single_action(hex_moveFront, 1)
             elif keyboard.is_pressed('s') or keyboard.is_pressed('2'):  # 向后移动，刮板和水系统停止
-                get_one_err = single_action(hex_moveBack)
+                get_one_err = single_action(hex_moveBack, 1)
             elif keyboard.is_pressed('a') or keyboard.is_pressed('4'):  # 向左旋转，刮板和水系统停止
-                get_one_err = single_action(hex_rotateLeft)
+                get_one_err = single_action(hex_rotateLeft, 1)
             elif keyboard.is_pressed('d') or keyboard.is_pressed('6'):  # 向右旋转，刮板和水系统停止
-                get_one_err = single_action(hex_rotateRight)
+                get_one_err = single_action(hex_rotateRight, 1)
             elif keyboard.is_pressed('x') or keyboard.is_pressed('1'):  # 刮板向前，停止移动，水系统停止
-                get_one_err = single_action(hex_boardFront)
+                get_one_err = single_action(hex_boardFront, 1)
             elif keyboard.is_pressed('c') or keyboard.is_pressed('3'):  # 刮板向后，停止移动，水系统停止
-                get_one_err = single_action(hex_boardBack)
+                get_one_err = single_action(hex_boardBack, 1)
             elif keyboard.is_pressed('z') or keyboard.is_pressed('5'):  # 启动水系统，停止移动，抬起刮板
-                get_one_err = single_action(hex_sprayStart)
+                get_one_err = single_action(hex_sprayStart, 1)
             elif keyboard.is_pressed('q'):  # 结束运行，全部停止
                 print('结束运行')
-                get_stop_err = single_action(hex_allStop)
+                get_stop_err = single_action(hex_allStop, 1)
                 is_oneAction = False
             else:  # 无输入，停止移动，刮板和水系统停止
-                get_stop_err = single_action(hex_allStop)
+                get_stop_err = single_action(hex_allStop, 1)
             # 无反馈退出
             if get_one_err == 98 or get_stop_err == 98:
                 print('无反馈，结束运行')
